472f0878b56d13ad305249e3d42629d1720ac0e7
PratikaNG <pratika.05ng@gmail.com>
2023-03-14 20:33:32 +0530

    states and props

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "Blog1", body: "abc", author: "Author1", id: 1 },
-    { title: "Blog2", body: "def", author: "Author2", id: 2 },
+    { title: "Blog2", body: "def", author: "Author1", id: 2 },
     { title: "Blog3", body: "ghi", author: "Author3", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 };
